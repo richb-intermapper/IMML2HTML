835c8b782280af88472a83acddaec558361a1603
Rich Brown <richard.e.brown@dartware.com>
2012-10-06 17:18:12 -0400

    Handle case of  \b\ …\$ … \$ … \$ … \p\

--- IMML2HTML.py
+++ IMML2HTML.py
@@ -10,9 +10,9 @@ import re
 crlf        = "\r\n"
 cr          = "\r"
 lf          = "\n"
-pTag        = '<p class="proberef">'
+pTag        = '<p class="content">'
 closepTag   = '</p>'
-liTag       = '<li class="proberef">'
+liTag       = '<li class="content">'
 closeliTag  = '</li>'
 
 def CleanLineEndings(aLine):
@@ -23,27 +23,26 @@ def CleanLineEndings(aLine):
     str = aLine.replace(cr, "")       # remove cr
     str = str.replace(lf, "")         # remove lf
     return str
-    
+
 def CleanMarkup(markup, urlString, tag):
 
     """
     The 'markup' string has the IMML tags
     Remove the named tags (usually m & g), as well as +, -, 0-9
+    Return "`" + markup + urlString + "`" or "" if nothing left
     """
-    result = markup
+    result = ""
     chars = set("0123456789+-" + tag)
     i = 0
-    while i < len(result):
-        if result[i] in chars:
-            result = result[:i] + result[(i+1):]
-        else:
-            i += 1
+    for i in range(len(markup)):
+        if markup[i] not in chars:
+            result += markup[i]
     result = result + urlString
     if result != "":
         result = "`" + result + "`"
     # print "Cleaned markup: '" + markup + "|" + result + "' <br />"
     return result
-    
+
 '''
 ProcessPropMono - scan the line, looking for "g" and "m" markup
 Emit the proper <code> and </code> tags, 
@@ -51,15 +50,32 @@ and remove the "g" and "m" markup from the returned line
 '''
 def ProcessPropMono(bLine):
     cLine = ""
-    inMarkup = False
+    inMarkup = False                    # we're actually processing markup commands
+    sawEscape = False                    # have seen opening "`", need to look at next character
     inMono = False
     markup = ""                         # contains the markup characters up to a "=" (in URL)
     urlStr = ""                         # if present, contains "=" and the URL
-    for c in bLine:						# character by character scan of the line
-        if c == "\xb2" or c == "\xb3":	# fix up <= or >= markup from ancient probes
+    markupchars = set("mg+-0123456789pbiu")
+    # print bLine
+    for c in bLine:                        # character by character scan of the line
+        if c == "\xb2" or c == "\xb3":    # fix up <= or >= markup from ancient probes
             c = "`"
-        if c == "`":                    # start/end markup section
+        # print "char is: '%s'" % c
+        
+        if sawEscape:                 # we have seen the "`" and need to look at this character
+            sawEscape = False         # we will handle it either way
+            d = c.lower()
+            if d in markupchars:     # it's legitimate markup
+                # print "In Markup: '%s', line is: '%s'" % (d, cLine)
+                inMarkup = True         # initialize the markup stuff
+                markup = c
+                urlStr = ""
+            else:                     # Not really markup - the "`" was just escaping this character
+                cLine += "`" + c     # and push it onto the output stream
+                # print "Found Escape: '%s', line is '%s'" % (d, cLine)
+        elif c == "`":                    # start/end markup section
             if inMarkup:                # if we're already within markup
+                # print "in Markup: '%s', '%s'" % (c, markup)
                 inMarkup = False        # this is the end
                 # Time to output the markup
                 markup = markup.lower() # make lower case
@@ -76,12 +92,11 @@ def ProcessPropMono(bLine):
                     cLine += markup
                 else:                    # regular markup
                     cLine += "`" + markup + urlStr + "`"
-                c = markup = urlStr = "" # and clear accumulated markup 
-                #print "The Line: '" + cLine + "'"
-            else:
-                inMarkup = True           # we're starting the markup
-                markup= ""                # clear accumulator
-                urlStr = ""
+                c = markup = urlStr = "" # and clear accumulated markup            
+            else:                          # we've seen the opening "`"
+                # print "saw escape:"
+                sawEscape = True          # Remember we we've seen the escape
+                # print "The Line: '" + cLine + "'"
         elif len(urlStr) != 0:            # we're accumulating the URL string...            
             urlStr += c
         elif inMarkup:                    # Not a "`" but we're in markup
@@ -93,6 +108,8 @@ def ProcessPropMono(bLine):
         else:
             cLine += c                    # append the character to the output string
         #print "[" + markup + "]" + cLine + "<br />"
+
+    # end of the character-by-character loop
     if markup != "":
         cLine += "`" + markup             # append any accumulated markup
     if inMono:
@@ -102,32 +119,32 @@ def ProcessPropMono(bLine):
     if cLine == "":                       # If we stripped out the entire line
         return None                       # return None
     return cLine                          # otherwise, return the line
-    
+
 def IMMLtoHTML(aLine):
-    
-    """
-	Emit reasonably attractive HTML code for this line
-	A charactistic of IMML is that it tends to have one line to a paragraph
-	Thus this function returns a line of <p ...> <the line> </p> lf
-	
-	IMML markup uses backslashes to delimit the IMML tags
-	 (and also Mac <= and >= characters in ancient probes)
-	Replace all "\" with "`" for ease of RE processing
-	Put back all backslashes when complete
-	
-	Remove Proportional/Monospace markings from IMML within this line
-	Assume we're in proportional ("G"). If \...m...\ encountered,
-	   switch to monospace and follow by with '<code>'
-	when we encounter closing \...g...\, precede with </code> and continue on
-	when this is done, remove "g" & "m" from markup tags
-	
-	THIS CODE IS NOT VERY ROBUST! It works for all built-in probes, and
-	  many sensible cases, but stressing IMML will generate inaccurate 
-	  HTML representations
 
     """
-    aLine = CleanLineEndings(aLine)		# remove line endings
-    if aLine == "":                    	# ignore (strip out) empty line 
+    Emit reasonably attractive HTML code for this line
+    A charactistic of IMML is that it tends to have one line to a paragraph
+    Thus this function returns a line of <p ...> <the line> </p> lf
+
+    IMML markup uses backslashes to delimit the IMML tags
+     (and also Mac <= and >= characters in ancient probes)
+    Replace all "\" with "`" for ease of RE processing
+    Put back all backslashes when complete
+
+    Remove Proportional/Monospace markings from IMML within this line
+    Assume we're in proportional ("G"). If \...m...\ encountered,
+       switch to monospace and follow by with '<code>'
+    when we encounter closing \...g...\, precede with </code> and continue on
+    when this is done, remove "g" & "m" from markup tags
+
+    THIS CODE IS NOT VERY ROBUST! It works for all built-in probes, and
+      many sensible cases, but stressing IMML will generate inaccurate
+      HTML representations
+
+    """
+    aLine = CleanLineEndings(aLine)        # remove line endings
+    if aLine == "":                        # ignore (strip out) empty line 
         return None
     if aLine.find("--") == 0:           # ignore comment at the start of line
         return None
@@ -135,31 +152,31 @@ def IMMLtoHTML(aLine):
     bLine = aLine.replace("\\", "`")    # convert '\' to '`' for parsing
 
     # Set up constant regular expressions
-    bPat =  re.compile("`[-+b0-9]+?`(.*?)`[-+P0-9]+?`", re.I)        	# bold text
-    iPat =  re.compile("`[-+i0-9]+?`(.*?)`[-+P0-9]+?`", re.I)        	# italic text
-    ibPat = re.compile("`[-+ib0-9]+?`(.*?)`[-+P0-9]+?`", re.I)        	# italic & bold
-    uPat =  re.compile("`[-+u0-9ib]+?`(.*?)`[-+P0-9]+?`", re.I)        	# just underlined text
-    uhPat = re.compile("`[-+u0-9ib]+?`(http)(.*?)`[-+P0-9]+?`", re.I) 	# URL between markup
-    uePat = re.compile("`[-+u0-9ib]+?=(.*?)`(.*?)`[-+P0-9]+?`", re.I) 	# URL within markup
-    pPat =  re.compile("`[-+p0-9]+?`(.*?)`[-+P0-9]+?`", re.I)        	# \p\ ... \p\
+    bPat =  re.compile("`[-+b0-9]+?`(.*?)`[-+P0-9]+?`", re.I)            # bold text
+    iPat =  re.compile("`[-+i0-9]+?`(.*?)`[-+P0-9]+?`", re.I)            # italic text
+    ibPat = re.compile("`[-+ib0-9]+?`(.*?)`[-+P0-9]+?`", re.I)            # italic & bold
+    uPat =  re.compile("`[-+u0-9ib]+?`(.*?)`[-+P0-9]+?`", re.I)            # just underlined text
+    uhPat = re.compile("`[-+u0-9ib]+?`(http)(.*?)`[-+P0-9]+?`", re.I)     # URL between markup
+    uePat = re.compile("`[-+u0-9ib]+?=(.*?)`(.*?)`[-+P0-9]+?`", re.I)     # URL within markup
+    pPat =  re.compile("`[-+p0-9]+?`(.*?)`[-+P0-9]+?`", re.I)            # \p\ ... \p\
 
-    cLine = ProcessPropMono(bLine)									# handle prop. and monospace
+    cLine = ProcessPropMono(bLine)                                    # handle prop. and monospace
     if cLine == None:
         return None
-    cLine = re.sub(iPat,  r"<i>\1</i>", cLine)                 		# italics
-    cLine = re.sub(bPat,  r"<b>\1</b>", cLine)                 		# bolds
-    cLine = re.sub(ibPat, r"<b><i>\1</i></b>", cLine)         		# bold italics
-    cLine = re.sub(uePat, r'<a href="\1">\2</a>', cLine)    		# u=... (has URL within markup)
-    cLine = re.sub(uhPat, r'<a href="\1\2">\1\2</a>', cLine)    	# u with URL (markup brackets URL)
-    cLine = re.sub(uPat,  r'<u>\1</u>', cLine)                		# u... (underlined text)
-    cLine = re.sub(pPat,  r'\1', cLine)                        		# \p\ ... \p\ is a no-op
+    cLine = re.sub(iPat,  r"<i>\1</i>", cLine)                         # italics
+    cLine = re.sub(bPat,  r"<b>\1</b>", cLine)                         # bolds
+    cLine = re.sub(ibPat, r"<b><i>\1</i></b>", cLine)                 # bold italics
+    cLine = re.sub(uePat, r'<a href="\1">\2</a>', cLine)            # u=... (has URL within markup)
+    cLine = re.sub(uhPat, r'<a href="\1\2">\1\2</a>', cLine)        # u with URL (markup brackets URL)
+    cLine = re.sub(uPat,  r'<u>\1</u>', cLine)                        # u... (underlined text)
+    cLine = re.sub(pPat,  r'\1', cLine)                                # \p\ ... \p\ is a no-op
 
     # print "'%s'" % aLine
     # print "'%s'" % cLine
-    if cLine[0] == "*" or cLine[0] == "-":            	# bullet-ish character at front of line
+    if cLine[0] == "*" or cLine[0] == "-":                # bullet-ish character at front of line
         cLine = cLine[1:]
-        cLine = liTag + cLine + closeliTag            	# wrap in <li> ... </li> tags
-    cLine = cLine.replace("`", "\\")                	# finally put back bare '\'s
+        cLine = liTag + cLine + closeliTag                # wrap in <li> ... </li> tags
+    cLine = cLine.replace("`", "\\")                    # finally put back bare '\'s
     cLine = pTag + cLine + closepTag
     return cLine
 
